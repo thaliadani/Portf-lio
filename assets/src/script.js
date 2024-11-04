6a24bccffc4458aad45a4ab96e2bdcaa4fd0fe9f
thaliadani <thaliadani2@gmail.com>
2024-11-04 10:54:17 -0800

    modified:   assets/src/script.js 	modified:   assets/styles/main.css 	modified:   assets/styles/main.css.map 	modified:   assets/styles/main.scss

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,8 +1,19 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    centeredSlides: true,
     slidesPerView: "auto",
+    spaceBetween: 10,
+    loop: true,
+    breakpoints: {
+        667: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        835: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
